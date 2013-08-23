e13589f71528d3d1d7377d7775db7c7eeb69b0e5
Aidan Lloyd <lloyd.aidan@gmail.com>
2013-08-23 17:30:56 -0400

    change -reset_ix flag to -start_ix flag

--- position.c
+++ position.c
@@ -14,7 +14,7 @@ char *position_strings[] = {
 #define MAX_LINE_LEN 1024
 static char line[MAX_LINE_LEN];
 
-static char usage[] = "usage: position (-debug) (-verbose) (-reset_ixix) multiplier commission filename\n";
+static char usage[] = "usage: position (-debug) (-verbose) (-start_ixix) multiplier commission filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
@@ -24,8 +24,8 @@ int main(int argc,char **argv)
   int curr_arg;
   bool bDebug;
   bool bVerbose;
-  bool bHaveResetIx;
-  int reset_ix;
+  bool bHaveStartIx;
+  int start_ix;
   double multiplier;
   double commission;
   char pos;
@@ -34,7 +34,6 @@ int main(int argc,char **argv)
   int sold_at;
   int temp;
   double position;
-  double reset_position;
   int points;
   int sum_points;
   double current;
@@ -49,16 +48,16 @@ int main(int argc,char **argv)
 
   bDebug = false;
   bVerbose = false;
-  bHaveResetIx = false;
+  bHaveStartIx = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-debug"))
       bDebug = true;
     else if (!strcmp(argv[curr_arg],"-verbose"))
       bVerbose = true;
-    else if (!strncmp(argv[curr_arg],"-reset_ix",9)) {
-      sscanf(&argv[curr_arg][9],"%d",&reset_ix);
-      bHaveResetIx = true;
+    else if (!strncmp(argv[curr_arg],"-start_ix",9)) {
+      sscanf(&argv[curr_arg][9],"%d",&start_ix);
+      bHaveStartIx = true;
     }
     else
       break;
@@ -78,10 +77,6 @@ int main(int argc,char **argv)
   }
 
   position = (double)0;
-
-  if (bHaveResetIx)
-    reset_position = (double)0;
-
   sum_points = 0;
   line_no = 0;
 
@@ -91,38 +86,35 @@ int main(int argc,char **argv)
     if (feof(fptr))
       break;
 
-    sscanf(line,"%c %d %d",&pos,&bought_at,&sold_at);
+    if (bHaveStartIx && (line_no >= start_ix)) {
+      sscanf(line,"%c %d %d",&pos,&bought_at,&sold_at);
 
-    if (pos == 'L')
-      ePos = POS_LONG;
-    else if (pos == 'S')
-      ePos = POS_SHORT;
-    else {
-      printf("unknown position character %c at line %d\n",pos,line_no+1);
-      return 4;
-    }
+      if (pos == 'L')
+        ePos = POS_LONG;
+      else if (pos == 'S')
+        ePos = POS_SHORT;
+      else {
+        printf("unknown position character %c at line %d\n",pos,line_no+1);
+        return 4;
+      }
 
-    if (ePos == POS_SHORT)
-      points = bought_at - sold_at;
-    else
-      points = sold_at - bought_at;
-
-    sum_points += points;
-
-    current = ((double)-2 * commission) + multiplier * (double)points;
-
-    position += current;
-
-    if (bHaveResetIx && (line_no >= reset_ix))
-      reset_position += current;
-
-    if (bDebug) {
-      if (!bVerbose)
-        printf("%lf\n",position);
-      else if (!bHaveResetIx || (line_no < reset_ix))
-        printf("%lf %s %d %d %d %d %lf\n",position,position_strings[ePos],bought_at,sold_at,sum_points,points,current);
+      if (ePos == POS_SHORT)
+        points = bought_at - sold_at;
       else
-        printf("%lf %s %d %d %d %d %lf %lf\n",position,position_strings[ePos],bought_at,sold_at,sum_points,points,current,reset_position);
+        points = sold_at - bought_at;
+
+      sum_points += points;
+
+      current = ((double)-2 * commission) + multiplier * (double)points;
+
+      position += current;
+
+      if (bDebug) {
+        if (!bVerbose)
+          printf("%lf\n",position);
+        else
+          printf("%lf %s %d %d %d %d %lf\n",position,position_strings[ePos],bought_at,sold_at,sum_points,points,current);
+      }
     }
 
     line_no++;
