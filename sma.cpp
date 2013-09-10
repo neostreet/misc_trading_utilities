1bb168495ef965bfe652d22ab4dac5e1c617f9ac
Aidan Lloyd <lloyd.aidan@gmail.com>
2013-09-10 15:01:23 -0400

    only use double precision

--- sma.cpp
+++ sma.cpp
@@ -1,13 +1,12 @@
 #include <vector>
 #include <stdio.h>
-#include <string.h>
 
 using namespace std;
 
 #define MAX_LINE_LEN 1024
 static char line[MAX_LINE_LEN];
 
-static char usage[] = "usage: sma (-truncate) num_periods filename\n";
+static char usage[] = "usage: sma num_periods filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
@@ -16,42 +15,24 @@ int main(int argc,char **argv)
 {
   int m;
   int n;
-  int curr_arg;
-  bool bTruncate;
   int num_periods;
   FILE *fptr;
   int line_len;
   int line_no;
-  int value;
-  vector<int> values;
-  int sma_count;
   double dwork;
-  int work;
+  vector<double> values;
+  int sma_count;
 
-  if ((argc < 3) || (argc > 4)) {
+  if (argc != 3) {
     printf(usage);
     return 1;
   }
 
-  bTruncate = false;
+  sscanf(argv[1],"%d",&num_periods);
 
-  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
-    if (!strcmp(argv[curr_arg],"-truncate"))
-      bTruncate = true;
-    else
-      break;
-  }
-
-  if (argc - curr_arg != 2) {
-    printf(usage);
+  if ((fptr = fopen(argv[2],"r")) == NULL) {
+    printf(couldnt_open,argv[2]);
     return 2;
-  }
-
-  sscanf(argv[curr_arg],"%d",&num_periods);
-
-  if ((fptr = fopen(argv[curr_arg+1],"r")) == NULL) {
-    printf(couldnt_open,argv[curr_arg+1]);
-    return 3;
   }
 
   line_no = 0;
@@ -64,40 +45,27 @@ int main(int argc,char **argv)
 
     line_no++;
 
-    sscanf(line,"%d",&value);
-    values.push_back(value);
+    sscanf(line,"%lf",&dwork);
+    values.push_back(dwork);
   }
 
   fclose(fptr);
 
   if (line_no < num_periods) {
     printf("not enough data points\n");
-    return 4;
+    return 3;
   }
 
   sma_count = line_no - num_periods + 1;
 
   for (n = 0; n < sma_count; n++) {
-    if (!bTruncate)
-      dwork = (double)0;
-    else
-      work = 0;
+    dwork = (double)0;
 
-    for (m = 0; m < num_periods; m++) {
-      if (!bTruncate)
-        dwork += (double)values[n+m];
-      else
-        work += values[n+m];
-    }
+    for (m = 0; m < num_periods; m++)
+      dwork += values[n+m];
 
-    if (!bTruncate) {
-      dwork /= (double)num_periods;
-      printf("%lf\n",dwork);
-    }
-    else {
-      work /= num_periods;
-      printf("%d\n",work);
-    }
+    dwork /= (double)num_periods;
+    printf("%lf\n",dwork);
   }
 
   return 0;
