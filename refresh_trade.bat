0b718206f7b182d3d42fb136d25cc3e11a018753
Aidan Lloyd <lloyd.aidan@gmail.com>
2013-09-24 14:23:00 -0400

    enable population of trade table from arbitrary orders file

--- refresh_trade.bat
+++ refresh_trade.bat
@@ -1,7 +1,7 @@
 @echo off
-call gen_insert_trade_it futures1_orders
+call gen_insert_trade_it futures1_orders.txt%1
 call tomysql < zap_trade.sql
-call tomysql < futures1_orders.gen_insert_trade.sql
+call tomysql < futures1_orders.txt%1.gen_insert_trade.sql
 mysql_grab trades
 mysql_grab trades2
 mysql_grab position
