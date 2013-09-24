9affb961547991e4bbf19f5328523267f3715b6f
Aidan Lloyd <lloyd.aidan@gmail.com>
2013-09-23 21:26:00 -0400

    add entry_order_type and exit_order_type

--- trades2.sql
+++ trades2.sql
@@ -1,6 +1,6 @@
 use trading
 
-select type,entry_filled,entry_filled_price,exit_filled,exit_filled_price,ticks,delta
+select type,entry_filled,entry_filled_price,exit_filled,exit_filled_price,ticks,delta,entry_order_type,exit_order_type
 from trade
 order by id;
 
